b95efe322e5df431f9d426aec8d0c4cc5b708ad7
erkamow <erkamafsin@afsteknik.com>
2020-08-09 11:00:29 -0400

    day39_CustomClass

---
+++ src/day39_CustomClass/Car.java
@@ -0,0 +1,17 @@
+package day39_CustomClass;
+
+public class Car {
+    /*
+    Attributes:
+    model,brand,year,color,mileage,price...
+     */
+
+
+    String model;
+    String brand;
+    String color;
+    int year;
+    double mileage;
+    double price;
+
+}
