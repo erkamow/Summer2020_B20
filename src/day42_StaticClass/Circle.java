65e3ee934657a89e5d1f48077f9dddd5b96c6295
erkamow <erkamafsin@afsteknik.com>
2020-08-16 14:55:56 -0400

    day42_StaticClass

---
+++ src/day42_StaticClass/Circle.java
@@ -0,0 +1,22 @@
+package day42_StaticClass;
+
+public class Circle {
+
+    double radius;
+    double diameter;
+    static  double pi = 3.14;
+
+    public void setInfo(double radius){
+        this.radius = radius;
+        diameter = radius *2;
+    }
+    public double calcArea(){
+        return radius * pi;
+    }
+    public double calcPerimeter(){
+        return diameter * pi;
+    }
+    public String toString(){
+        return "Radius: "+radius+"\nDiameter:"+diameter+"\nPI: "+pi+"\nArea: "+calcArea()+"\nPerimeter: "+calcPerimeter();
+    }
+}
